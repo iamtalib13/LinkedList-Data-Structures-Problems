18bea701c7c76d41b7cf2e1ccfc7082a9746b745
iamtalib13 <talibsheikh16@gmail.com>
2022-08-21 00:36:34 +0530

    Added -Appending 30 and 70 to 56

--- src/com/bridgelabz/linkedlist/LinkedListDemo.java
+++ src/com/bridgelabz/linkedlist/LinkedListDemo.java
@@ -26,6 +26,18 @@ class LinkedList {
         }
     }
 
+    //Insert Data from Last
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+        if(tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
     //Display Node in Linked List
     public void showLinkedList() {
         if(head == null) {
@@ -43,14 +55,19 @@ class LinkedList {
 public class LinkedListDemo {
     public static void main(String[] args) {
         //Welcome Message
-        System.out.println("Welcome to LinkedList Data Structures Problem.");
+        System.out.println("Welcome to LinkedList Data Structures Problems.");
 
         LinkedList linkedList = new LinkedList();
 
         //Insert 70, 50, 36 in Linked List Nodes
-        linkedList.insertFirst(70);
-        linkedList.insertFirst(30);
-        linkedList.insertFirst(56);
+// 	  linkedList.insertFirst(70);
+//        linkedList.insertFirst(30);
+//        linkedList.insertFirst(56);
+        linkedList.insertLast(56);
+        linkedList.insertLast(30);
+        linkedList.insertLast(70);
+
+
 
         //Showing Linked List Data
         linkedList.showLinkedList();
